e8e3010e2cedb77701f5562eedde22f1c2c786ed
Jenna Link <jenna.hartley@live.ca>
2021-01-10 12:09:43 -0600

    Part 1c

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,61 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Part = (props) => {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  )
-}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-const Content = (props) => {
-  let parts = props.parts
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
 
-const Total = (props) => {
-  let parts = props.parts
-  return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const handleClick = () => {
+    console.log('clicked')
   }
 
+  const plusOne = () => setCounter(counter + 1)
+  const minusOne = () => setCounter(counter - 1)
+  const toZero = () => setCounter(0)
+
   return (
     <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
+      <Display counter={counter} />
+      <Button
+        handleClick={plusOne}
+        text='plus'/>
+      <Button
+        handleClick={toZero}
+        text='zero'/>
+      <Button
+        handleClick={minusOne}
+        text='minus'/>
     </div>
   )
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+
+let counter = 1
+
+ReactDOM.render(
+  <App counter={counter} />,
+  document.getElementById('root')
+)
