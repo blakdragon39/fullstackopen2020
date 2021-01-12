d663f1e1c816f71cd3f182fbca2b169b01aff97d
Jenna Hartley <jenna.hartley@pushinteractions.com>
2021-01-11 20:42:29 -0600

    Exercise 2.9

--- part2/src/App.js
+++ part2/src/App.js
@@ -12,9 +12,11 @@ const App = () => {
     ])
     const [ newName, setNewName ] = useState('')
     const [ newPhone, setNewPhone ] = useState('')
+    const [ filter, setFilter ] = useState('')
 
     const handleNameChange = (event) => setNewName(event.target.value)
     const handlePhoneChange = (event) => setNewPhone(event.target.value)
+    const handleFilterChange = (event) => setFilter(event.target.value)
 
     const submitNewName = (event) => {
         event.preventDefault()
@@ -31,13 +33,19 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>filter on <input onChange={handleFilterChange}/></div>
+            <h2>Add New</h2>
             <form onSubmit={submitNewName}>
                 <div>name: <input value={newName} onChange={handleNameChange}/></div>
                 <div>number: <input value={newPhone} onChange={handlePhoneChange}/></div>
                 <div><button type="submit">add</button></div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => <Person person={person}/>)}
+            {
+                persons
+                    .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+                    .map(person => <Person person={person}/>)
+            }
         </div>
     )
 }
