978c9b10731592b9bec428b9b140ce2380da17f5
Jenna Hartley <jenna.hartley@pushinteractions.com>
2021-01-13 17:11:56 -0600

    Exercise 2.13

--- part2/part2-countries/src/App.js
+++ part2/part2-countries/src/App.js
@@ -1,14 +1,19 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Filter = ({handleQueryChange}) => <div>find countries <input onChange={handleQueryChange}/></div>
+const Filter = ({query, handleQueryChange}) =>
+    <div>find countries <input value={query} onChange={handleQueryChange}/></div>
 
 const TooMany = () => <div>Too many matches, specify another filter</div>
 
-const CountryName = ({country}) => <div>{country.name}</div>
+const CountryPreview = ({country, handleShowCountry}) => {
+    return (<div>{country.name}
+        <button onClick={() => handleShowCountry(country)}>show</button>
+    </div>)
+}
 
 const Country = ({country}) => {
-    return(
+    return (
         <div>
             <h2>{country.name}</h2>
             <p>
@@ -18,23 +23,30 @@ const Country = ({country}) => {
             <h3>Languages</h3>
             <ul>
                 {
-                    country.languages.map(language => <li key={language.name}>{language.name}</li> )
+                    country.languages.map(language => <li key={language.name}>{language.name}</li>)
                 }
             </ul>
-            <img src={country.flag} height="200px"/>
+            <img src={country.flag} height="200px" alt="flag"/>
         </div>
     )
 }
 
-const Countries = ({countries, query}) => {
+const Countries = ({countries, query, handleShowCountry}) => {
     const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(query.toLowerCase()))
 
     if (filteredCountries.length === 1) {
-        return (<Country country={filteredCountries[0]} /> )
+        return (<Country country={filteredCountries[0]}/>)
     } else if (filteredCountries.length <= 10) {
         return (
             <div>
-                {filteredCountries.map(country => <CountryName country={country} key={country.name}/>)}
+                {
+                    filteredCountries
+                        .map(country =>
+                            <CountryPreview
+                                country={country}
+                                key={country.name}
+                                handleShowCountry={handleShowCountry}/>)
+                }
             </div>
         )
     } else {
@@ -48,6 +60,8 @@ const App = () => {
 
     const handleQueryChange = (event) => setQuery(event.target.value)
 
+    const handleShowCountry = (country) => setQuery(country.name)
+
     useEffect(() => {
         axios.get('https://restcountries.eu/rest/v2/all')
             .then(response => setCountries(response.data))
@@ -55,8 +69,8 @@ const App = () => {
 
     return (
         <div>
-            <Filter handleQueryChange={handleQueryChange}/>
-            <Countries countries={countries} query={query}/>
+            <Filter query={query} handleQueryChange={handleQueryChange}/>
+            <Countries countries={countries} query={query} handleShowCountry={handleShowCountry}/>
         </div>
     );
 }
