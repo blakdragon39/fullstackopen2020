7602f1ba82cc03189cfb5bde22b638bf53c39b49
Jenna Hartley <jenna.hartley@pushinteractions.com>
2021-02-15 16:04:37 -0600

    Exercise 6.9

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { addVote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './componenets/AnecdoteForm'
 import AnecdoteList from './componenets/AnecdoteList'
 
 const App = () => {
-
-
     return (
         <div>
             <h2>Anecdotes</h2>
