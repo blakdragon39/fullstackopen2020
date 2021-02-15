d5e7bbdf9b22c6055609976686751ce63b5526c1
Jenna Hartley <jenna.hartley@pushinteractions.com>
2021-02-15 13:06:14 -0600

    Exercise 6.8

--- part6/src/App.js
+++ part6/src/App.js
@@ -2,28 +2,16 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addVote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './componenets/AnecdoteForm'
+import AnecdoteList from './componenets/AnecdoteList'
 
 const App = () => {
-    const anecdotes = useSelector(state => state)
-    const dispatch = useDispatch()
 
-    const vote = (id) => dispatch(addVote(id))
 
     return (
         <div>
             <h2>Anecdotes</h2>
-            {anecdotes.map(anecdote =>
-                <div key={anecdote.id}>
-                    <div>
-                        {anecdote.content}
-                    </div>
-                    <div>
-                        has {anecdote.votes}
-                        <button onClick={() => vote(anecdote.id)}>vote</button>
-                    </div>
-                </div>
-            )}
             <AnecdoteForm />
+            <AnecdoteList />
         </div>
     )
 }
