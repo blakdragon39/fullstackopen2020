452e660a7ce76af3e91ed6dd4a5162768ed0756f
Jenna Link <jenna.hartley@pushinteractions.com>
2021-02-22 13:46:01 -0600

    Exercise 10.5

--- part10/components/RepositoryList.js
+++ part10/components/RepositoryList.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { FlatList, View, StyleSheet } from 'react-native';
+import { Image, FlatList, View, StyleSheet } from 'react-native';
 import Text from './Text';
-
-const styles = StyleSheet.create({
-    separator: {
-        height: 1,
-        backgroundColor: '#000000',
-    },
-});
+import theme from '../theme';
 
 const repositories = [
     {
@@ -60,9 +54,38 @@ const ItemSeparator = () => <View style={styles.separator} />;
 
 const Item = ({ item, index, separators }) => {
     return (
-        <View>
-            <Text fontWeight="bold">{item.fullName}</Text>
-            <Text>{item.description}</Text>
+        <View style={styles.cell}>
+            <View style={styles.topSection}>
+                <Image
+                    style={styles.avatar}
+                    source={{uri: item.ownerAvatarUrl}}/>
+                <View style={styles.details} >
+                    <Text style={styles.spacing} fontWeight="bold">{item.fullName}</Text>
+                    <Text style={styles.spacing}>{item.description}</Text>
+                    <Text style={[styles.languageLabel, styles.spacing]}>{item.language}</Text>
+                </View>
+            </View>
+            <View style={styles.bottomSection}>
+                <Stat number={item.stargazersCount} label="Stars" />
+                <Stat number={item.forksCount} label="Forks" />
+                <Stat number={item.reviewCount} label="Reviews" />
+                <Stat number={item.ratingAverage} label="Rating" />
+            </View>
+        </View>
+    );
+};
+
+const Stat = ({ number, label }) => {
+    if (number >= 1000) {
+        number -= number % 100;
+        number = number / 1000;
+        number += "K";
+    }
+
+    return (
+        <View style={styles.stat}>
+            <Text fontWeight="bold">{number}</Text>
+            <Text>{label}</Text>
         </View>
     );
 };
@@ -70,10 +93,54 @@ const Item = ({ item, index, separators }) => {
 const RepositoryList = () => {
     return (
         <FlatList
+            style={styles.container}
             data={repositories}
             ItemSeparatorComponent={ItemSeparator}
             renderItem={Item}/>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#c4c4c4',
+    },
+    separator: {
+        height: 12,
+    },
+    cell: {
+        backgroundColor: '#ffffff',
+        padding: 16,
+    },
+    topSection: {
+        flexDirection: 'row',
+        marginBottom: 8,
+    },
+    bottomSection: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    avatar: {
+        width: 64,
+        height: 64,
+        marginEnd: 12,
+        borderRadius: 4,
+    },
+    details: {
+        flex: 1,
+    },
+    languageLabel: {
+        color: '#ffffff',
+        backgroundColor: theme.colors.primary,
+        borderRadius: 4,
+        padding: 6,
+        alignSelf: 'flex-start',
+    },
+    stat: {
+        alignItems: 'center',
+    },
+    spacing: {
+        marginBottom: 4,
+    },
+});
 
 export default RepositoryList;
